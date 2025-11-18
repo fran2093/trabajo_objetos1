7e2c7a111862e45fcae40414e1055170b8f39193
frahu <ferrofrancisco2093@gmail.com>
2025-11-18 17:54:08 -0300

    v2
    Metodos de cuenta + Menu Joptionpane en Main

--- src/trabajo_objetos1/Cuenta.java
+++ src/trabajo_objetos1/Cuenta.java
@@ -1,5 +1,7 @@
 package trabajo_objetos1;
 
+import javax.swing.JOptionPane;
+
 public class Cuenta {
 	private String mail;
 	private String clave;
@@ -13,6 +15,36 @@ public class Cuenta {
 		this.saldo = saldo;
 	}
 
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+            JOptionPane.showMessageDialog(null, 
+                "Deposito exitoso.\nNuevo saldo: " + saldo);
+        } else {
+            JOptionPane.showMessageDialog(null, 
+                "Monto invalido.");
+        }
+    }
+    
+    public boolean retirar(double monto) {
+        if (monto > saldo || monto <= 0) {
+            JOptionPane.showMessageDialog(null, 
+                "No es posible retirar ese monto.");
+            return false;
+        }
+        saldo -= monto; //aca le resto el saldo retirado al monto
+        JOptionPane.showMessageDialog(null, 
+            "Retiro exitoso.\nNuevo saldo: " + saldo);
+        return true; //se retiro correctamente jeje
+    }
+   
+    public void mostrarSaldo() {
+        JOptionPane.showMessageDialog(null, 
+            "Saldo actual: " + saldo);
+    }
+	
+	
+	
 	public String getMail() {
 		return mail;
 	}
